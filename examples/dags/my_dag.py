96231e9d05bbcb8202a5d80aa5b93aee76ae7b70
Todsapon Vittayasakoonchai <65076408+todsaponvit@users.noreply.github.com>
2026-09-02 00:13:53 +0700

    Enable Spark job submission in my_dag
    Uncommented SparkSubmitOperator and added it to the DAG.

--- examples/dags/my_dag.py
+++ examples/dags/my_dag.py
@@ -2,7 +2,7 @@
 from airflow.decorators import dag, task
 from datetime import datetime
 
-from pyspark import SparkContext
+#from pyspark import SparkContext
 from pyspark.sql import SparkSession
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 import pandas as pd
@@ -14,29 +14,13 @@ import pandas as pd
 )
 def my_dag():
     
-    # submit_job = SparkSubmitOperator(
-    #     task_id="submit_job",
-    #     conn_id="my_spark_conn",
-    #     application="include/scripts/read.py",
-    #     verbose=True,
-    # )
+    submit_job = SparkSubmitOperator(
+        task_id="submit_job",
+        conn_id="my_spark_conn",
+        application="include/scripts/read.py",
+        verbose=True,
+    )
     
-    # submit_job
-    
-    @task.pyspark(conn_id="my_spark_conn")
-    def read_data(spark: SparkSession, sc: SparkContext) -> pd.DataFrame:
-        df = spark.createDataFrame(
-            [
-                (1, "John Doe", 21),
-                (2, "Jane Doe", 22),
-                (3, "Joe Bloggs", 23),
-            ],
-            ["id", "name", "age"],
-        )
-        df.show()
-
-        return df.toPandas()
-    
-    read_data()
+    submit_job
 
 my_dag()
