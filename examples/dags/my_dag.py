3d0dea0a9f5fb18dc8a453855ede1a2ff07fc162
Todsapon Vittayasakoonchai <65076408+todsaponvit@users.noreply.github.com>
2026-09-02 00:46:22 +0700

    Replace SparkSubmitOperator with PySpark task
    Refactored DAG to use a PySpark task for reading data instead of SparkSubmitOperator.

--- examples/dags/my_dag.py
+++ examples/dags/my_dag.py
@@ -2,7 +2,7 @@
 from airflow.decorators import dag, task
 from datetime import datetime
 
-#from pyspark import SparkContext
+from pyspark import SparkContext
 from pyspark.sql import SparkSession
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 import pandas as pd
@@ -14,13 +14,20 @@ import pandas as pd
 )
 def my_dag():
     
-    submit_job = SparkSubmitOperator(
-        task_id="submit_job",
-        conn_id="my_spark_conn",
-        application="include/read_csv.py",
-        verbose=True,
-    )
+    @task.pyspark(conn_id="my_spark_conn")
+    def read_data(spark: SparkSession, sc: SparkContext) -> pd.DataFrame:
+        df = spark.createDataFrame(
+            [
+                (1, "John Doe", 21),
+                (2, "Jane Doe", 22),
+                (3, "Joe Bloggs", 23),
+            ],
+            ["id", "name", "age"],
+        )
+        df.show()
+
+        return df.toPandas()
     
-    submit_job
+    read_data()
 
 my_dag()
